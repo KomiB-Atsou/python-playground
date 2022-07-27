f03d931cb22630170a3c9ac6e4ceafb3bfd7bf53
KomiB-Atsou <marc.atsou@gmail.com>
2022-07-27 08:22:09 +0200

    Assignment operators / Augmented assignment

--- test_assignment.py
+++ test_assignment.py
@@ -21,3 +21,72 @@ def test_assignment_operator():
     first_variable, second_variable = second_variable, first_variable
     assert first_variable == 1
     assert second_variable == 0
+
+
+def test_augmented_assignment_operators():
+    """Assignment operator combined with arithmetic and bitwise operators"""
+
+    # Assignment: +=
+    number = 5
+    number += 3
+    assert number == 8
+
+    # Assignment: -=
+    number = 5
+    number -= 3
+    assert number == 2
+
+    # Assignment: *=
+    number = 5
+    number *= 3
+    assert number == 15
+
+    # Assignment: /=
+    number = 8
+    number /= 4
+    assert number == 2
+
+    # Assignment: %=
+    number = 8
+    number %= 3
+    assert number == 2
+
+    # Assignment: %=
+    number = 5
+    number %= 3
+    assert number == 2
+
+    # Assignment: //=
+    number = 5
+    number //= 3
+    assert number == 1
+
+    # Assignment: **=
+    number = 5
+    number **= 3
+    assert number == 125
+
+    # Assignment: &=
+    number = 5  # 0b0101
+    number &= 3  # 0b0011
+    assert number == 1  # 0b0001
+
+    # Assignment: |=
+    number = 5  # 0b0101
+    number |= 3  # 0b0011
+    assert number == 7  # 0b0111
+
+    # Assignment: ^=
+    number = 5  # 0b0101
+    number ^= 3  # 0b0011
+    assert number == 6  # 0b0110
+
+    # Assignment: >>=
+    number = 5
+    number >>= 3
+    assert number == 0  # (((5 // 2) // 2) // 2)
+
+    # Assignment: <<=
+    number = 5
+    number <<= 3
+    assert number == 40  # 5 * 2 * 2 * 2
